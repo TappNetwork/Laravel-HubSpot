3fc44f136e1a8235c09ac5269af0e2ec3c6ac15a
Steve Williamson <steve@sjdub.com>
2025-08-01 12:07:49 -0400

    add argument for arch

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -4,8 +4,8 @@ arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
 
-arch()->preset()->php();
+arch('php preset')->preset()->php();
 
-arch()->preset()->security();
+arch('security preset')->preset()->security();
 
-arch()->preset()->laravel();
+arch('laravel preset')->preset()->laravel();
