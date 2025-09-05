057f2e28ce9b912a1c5eeedce832307c61d58830
scott grayson <Scott Grayson>
2025-09-05 17:11:24 -0400

    CI linter updates

--- config/hubspot.php
+++ config/hubspot.php
@@ -5,6 +5,7 @@ return [
     'disabled' => env('HUBSPOT_DISABLED', false),
     'api_key' => env('HUBSPOT_TOKEN'),
     'log_requests' => env('HUBSPOT_LOG_REQUESTS', false),
+    'use_real_api' => env('HUBSPOT_USE_REAL_API', false),
     'property_group' => env('HUBSPOT_PROPERTY_GROUP', 'app_user_profile'),
     'property_group_label' => env('HUBSPOT_PROPERTY_GROUP_LABEL', 'App User Profile'),
 
